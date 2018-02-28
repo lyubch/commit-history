982e1f99036a67b6130c8e65bc955ff3db6d629d
Anton Lyubch <anton.lyubch@gmail.com>
2018-02-28 22:39:00 +0200

    modify readme

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# commit-history
+<p align="center">
+    <h1 align="center">Commit History App Yii1</h1>
+    <br>
+</p>
+
+## Installing using Vagrant
+
+This way is the easiest but long (~20 min).
+
+**This installation way doesn't require pre-installed software (such as web-server, PHP, MySQL etc.)** - just do next steps!
+
+#### Manual for Linux/Unix users
+
+1. Install [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
+2. Install [Vagrant](https://www.vagrantup.com/downloads.html)
+3. Prepare project:
+   
+   ```bash
+   git clone https://github.com/lyubch/commit-history.git
+   cd commit-history/vagrant/config
+   cp vagrant-local.example.yml vagrant-local.yml
+   ```
+   
+4. Edit settings at your `vagrant-local.yml` if need
+5. Change directory to project root:
+
+   ```bash
+   cd commit-history
+   ```
+
+5. Run command:
+
+   ```bash
+   vagrant up
+   ```
+   
+That's all. You just need to wait for completion! After that you can access project locally by URLs:
+* Application URL: http://commit-history.local
+* PhpMyAdmin URL: http://commit-history.local/phpmyadmin
+   
+#### Manual for Windows users
+
+1. Install [VirtualBox](https://www.virtualbox.org/wiki/Downloads)
+2. Install [Vagrant](https://www.vagrantup.com/downloads.html)
+3. Reboot
+4. Prepare project:
+   * download repo [phalcon-app](https://github.com/lyubch/commit-history/archive/master.zip)
+   * unzip it
+   * go into directory `commit-history-master/vagrant/config`
+   * copy `vagrant-local.example.yml` to `vagrant-local.yml`
+
+5. Edit settings at your `vagrant-local.yml` if need
+
+6. Open terminal (`cmd.exe`), **change directory to project root** and run command:
+
+   ```bash
+   vagrant up
+   ```
+   
+   (You can read [here](http://www.wikihow.com/Change-Directories-in-Command-Prompt) how to change directories in command prompt) 
+
+That's all. You just need to wait for completion! After that you can access project locally by URLs:
+* Application URL: http://commit-history.local
+* PhpMyAdmin URL: http://commit-history.local/phpmyadmin
--- protected/views/mail/commit-history.php
+++ protected/views/mail/commit-history.php
@@ -3,47 +3,41 @@
 <p><b>Features</b></p>
 <?php if (isset($commits[Commits::TYPE_FEATURE])): ?>
     <?php foreach ($commits[Commits::TYPE_FEATURE] as $commit): ?>
-        <?php
-        echo strtr('{task_id}: {description} {link} ({date})', array(
+        <?php echo strtr('{task_id}: {description} {link} ({date})', array(
             '{task_id}'     => $commit->task_id,
             '{description}' => $commit->description,
             '{link}'        => CHtml::link($commit->url, $commit->url),
             '{date}'        => $commit->date->format('m/d/Y'),
-        ));
-        ?><br />
-            <?php endforeach; ?>
-<?php else: ?>
-        - No recent commits -<br />
-        <?php endif; ?>
-
-        <p><b>Defects</b></p>
-        <?php if (isset($commits[Commits::TYPE_DEFECT])): ?>
-        <?php foreach ($commits[Commits::TYPE_DEFECT] as $commit): ?>
-            <?php
-            echo strtr('{task_id}: {description} {link} ({date})', array(
-                '{task_id}'     => $commit->task_id,
-                '{description}' => $commit->description,
-                '{link}'        => CHtml::link($commit->url, $commit->url),
-                '{date}'        => $commit->date->format('m/d/Y'),
-        ));
-        ?><br />
-            <?php endforeach; ?>
-<?php else: ?>
-        - No recent commits -<br />
-        <?php endif; ?>
-
-        <p><b>Changes</b></p>
-        <?php if (isset($commits[Commits::TYPE_CHANGE])): ?>
-        <?php foreach ($commits[Commits::TYPE_CHANGE] as $commit): ?>
-            <?php
-            echo strtr('{task_id}: {description} {link} ({date})', array(
-                '{task_id}'     => $commit->task_id,
-                '{description}' => $commit->description,
-                '{link}'        => CHtml::link($commit->url, $commit->url),
-                '{date}'        => $commit->date->format('m/d/Y'),
-        ));
-        ?><br />
+        )); ?><br />
     <?php endforeach; ?>
 <?php else: ?>
-        - No recent commits -<br />
-        <?php endif; ?>
+    - No recent commits -<br />
+<?php endif; ?>
+
+<p><b>Defects</b></p>
+<?php if (isset($commits[Commits::TYPE_DEFECT])): ?>
+    <?php foreach ($commits[Commits::TYPE_DEFECT] as $commit): ?>
+        <?php echo strtr('{task_id}: {description} {link} ({date})', array(
+                '{task_id}'     => $commit->task_id,
+                '{description}' => $commit->description,
+                '{link}'        => CHtml::link($commit->url, $commit->url),
+                '{date}'        => $commit->date->format('m/d/Y'),
+        )); ?><br />
+    <?php endforeach; ?>
+<?php else: ?>
+    - No recent commits -<br />
+<?php endif; ?>
+
+<p><b>Changes</b></p>
+<?php if (isset($commits[Commits::TYPE_CHANGE])): ?>
+    <?php foreach ($commits[Commits::TYPE_CHANGE] as $commit): ?>
+        <?php echo strtr('{task_id}: {description} {link} ({date})', array(
+                '{task_id}'     => $commit->task_id,
+                '{description}' => $commit->description,
+                '{link}'        => CHtml::link($commit->url, $commit->url),
+                '{date}'        => $commit->date->format('m/d/Y'),
+        )); ?><br />
+    <?php endforeach; ?>
+<?php else: ?>
+    - No recent commits -<br />
+<?php endif; ?>
